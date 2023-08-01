07bf6673889b93129ffae670ae1103ece662fc38
Fahim <fahimshahariar.fs@gmail.com>
2023-08-01 10:34:24 +0600

    first

---
+++ elearningapp1/lib/app_blocs.dart
@@ -0,0 +1,10 @@
+import 'package:elearningapp1/app_events.dart';
+import 'package:elearningapp1/app_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocs extends Bloc<AppEvents, AppStates>{
+  AppBlocs(): super (InitStates()){
+    on<Increment>((event, emit) => emit(AppStates(counter: state.counter+1)));
+  }
+
+}
